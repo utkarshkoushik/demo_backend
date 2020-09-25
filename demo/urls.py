30c14dcac8caf1b2e50e6b30b0433ec6ba455638
utkarshkoushik <utkarshkoushik007@gmail.com>
2020-09-25 14:37:53 +0530

    added admin

--- demo/urls.py
+++ demo/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import url 
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
+    url(r'^admin/', admin.site.urls),
     path('',include('demo_backend.urls')),
     
 ]
